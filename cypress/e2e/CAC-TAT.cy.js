7742a4eab281c6102051c54ba067016898dd816f
Syonet <renanvicente@gmail.com>
2025-02-20 14:03:17 -0300

    first commit

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,91 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+
+  //Ex 01.
+  beforeEach(() => {
+    cy.visit('./src/index.html')
+  })
+  
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('Teste', 20)
+
+    cy.get('#firstName').type('Renan')
+    cy.get('#lastName').type('Vicente')
+    cy.get('#email').type('renanvicente@gmail.com')
+    cy.get('#phone').type('99999999')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+
+  //Ex 02.
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Renan')
+    cy.get('#lastName').type('Vicente')
+    cy.get('#phone').type('99999999')
+    cy.get('#email').type('testeEmailInvalido')
+    cy.get('#open-text-area').type('teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  //Ex 03.
+  it('numero com valor não numérico permanece vazio', () => {
+    cy.get('#phone')
+      .type('testeValorNaoNumerico')
+      .should('have.value', '')
+  })
+
+  //Ex 04.
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Renan')
+    cy.get('#lastName').type('Vicente')
+    cy.get('#email').type('renanvicente@gmail.com')
+    cy.get('#open-text-area').type('teste')
+    cy.get('#phone-checkbox').click()
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  //Ex 05.
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('Renan')
+      .should('have.value', 'Renan')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#lastName')
+      .type('Vicente')
+      .should('have.value', 'Vicente')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#phone')
+      .type('99999999')
+      .should('have.value', '99999999')
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#email')
+      .type('renanvicente@gmail.com')
+      .should('have.value', 'renanvicente@gmail.com')
+      .clear()
+      .should('have.value', '')
+  })
+
+  //Ex 06.
+  it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+
+  })
+})
+
