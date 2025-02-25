181cdfde5a1b20894ee8e017c2c8b59228493315
Syonet <renanvicente@gmail.com>
2025-02-25 13:25:41 -0300

    adiciona exercício link2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -165,8 +165,18 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
   //Ex 06.
-  it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
-    cy.get('a')
-      .should('have.attr', 'target', '_blank')
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+  })
+
+  //Extra 01
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
   })
 })
